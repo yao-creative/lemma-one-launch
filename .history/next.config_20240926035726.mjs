e19007a05fb98220ff44aee37098e92ba1e99cb7
Yi Yao Tan <yytanwork@gmail.com>
2024-09-28 12:46:35 +0800

    additionally history cleaned

--- .history/next.config_20240926035726.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: [], // Add any external domains you need to load images from
-    },
-};
-
-export default nextConfig;
